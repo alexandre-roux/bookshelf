ef2ef6b18f8e345bac1f4c9e986ea27b93f751b9
Alexandre  Roux <aroux@softbankrobotics.com>
2024-02-27 16:40:18 +0100

    First commit

---
+++ app/src/main/java/com/example/bookshelf/ui/screens/BookshelfViewModel.kt
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.bookshelf.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.bookshelf.Bookshelf
+import com.example.bookshelf.data.BooksRepository
+import com.example.bookshelf.model.Book
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+/**
+ * UI state for the Home screen
+ */
+sealed interface BookshelfUiState {
+    data class Success(val books: List<Book>) : BookshelfUiState
+    object Error : BookshelfUiState
+    object Loading : BookshelfUiState
+}
+
+class BookshelfViewModel(private val booksRepository: BooksRepository) : ViewModel() {
+    /** The mutable State that stores the status of the most recent request */
+    var bookshelfUiState: BookshelfUiState by mutableStateOf(BookshelfUiState.Loading)
+        private set
+
+    /**
+     * Call getBooks() on init so we can display status immediately.
+     */
+    init {
+        getBooks()
+    }
+
+    /**
+     * Gets books information from the Books API Retrofit service and updates the
+     * [Book] [List] [MutableList].
+     */
+    fun getBooks() {
+        viewModelScope.launch {
+            bookshelfUiState = BookshelfUiState.Loading
+            bookshelfUiState = try {
+                BookshelfUiState.Success(booksRepository.getBooks())
+            } catch (e: IOException) {
+                BookshelfUiState.Error
+            } catch (e: HttpException) {
+                BookshelfUiState.Error
+            }
+        }
+    }
+
+    /**
+     * Factory for [BookshelfViewModel] that takes [BooksRepository] as a dependency
+     */
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as Bookshelf)
+                val booksRepository = application.container.booksRepository
+                BookshelfViewModel(booksRepository = booksRepository)
+            }
+        }
+    }
+}
